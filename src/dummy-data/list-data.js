a0c7787317a5d360af8512ee12e7fc428b1213cf
valor323 <caine101@mail.chapman.edu>
2018-09-19 15:05:32 -0700

    completed displaying dummy data

---
+++ src/dummy-data/list-data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'Go to store',
+        details: 'Get eggs, milkm bread, and diet coke',
+        _id: '001'
+    },
+    {
+        title: 'Wash car',
+        details: ' Find water and soap and brush and now scrub',
+        _id: '002'
+    },
+    {
+        title: ' Yell like a goat',
+        details: ' make high pitch noise and yell',
+        _id: '003'
+    },
+    {
+        title: 'Do laundry',
+        details: 'get clothes, put in washer, wait an hour',
+        _id: '004'
+    },
+]
