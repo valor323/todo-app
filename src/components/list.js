3bce0127ba8c0dd44fd5d65ffbe1f2eb64b50f7a
valor323 <caine101@mail.chapman.edu>
2018-09-19 16:32:11 -0700

    Completed add functionality

--- src/components/list.js
+++ src/components/list.js
@@ -1,33 +1,14 @@
-import React, {Component} from 'react';
-import dummylistdata from '../dummy-data/list-data';
+import React from 'react';
 
 
-class List extends Component{
 
-    state = {
-        list: []
-    }
-
-    componentDidMount(){
-        this.getListData();
-    }
-
-    getListData(){
-        //call server to get data
-
-        this.setState({
-            list: dummylistdata
-        })
-    }
-
-    render(){
-        const listElement = this.state.list.map((item, index)=>{
-            return <li className= 'collection-item' key={item._id}>{item.title}</li>
-        });
-        return (
-                <ul className = 'collection'>{listElement}</ul>
-        )
-    }
+const List = (props) => {
+    const listElement = props.data.map((item, index)=>{
+        return <li className= 'collection-item' key={item._id}>{item.title}</li>
+    });
+    return (
+            <ul className = 'collection'>{listElement}</ul>
+    )
 }
 
 export default List;
