7174b4c290bbde06bf1576e17c22cba983534581
valor323 <caine101@mail.chapman.edu>
2018-09-19 17:04:00 -0700

    clear form after item added

--- src/components/add-item.js
+++ src/components/add-item.js
@@ -10,6 +10,10 @@ class AddItem extends Component {
     handleAddItem(e){
         e.preventDefault();
         this.props.add(this.state);
+        this.setState({
+            title: '',
+            details: ''
+        })
     }
 
     render(){
